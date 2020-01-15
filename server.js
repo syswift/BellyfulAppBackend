1fc997e3b479b126986c3a621d5c3c089381b0e7
syswift <39137902+syswift@users.noreply.github.com>
2020-01-16 11:57:25 +1300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('client'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('post'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('Users'); //cc
 
 app.use(mobileApp);
