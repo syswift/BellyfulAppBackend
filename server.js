78011e999e3b4cfdf48269aa655c522f52b9ec0c
syswift <39137902+syswift@users.noreply.github.com>
2020-02-05 01:11:01 +1300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 mobileApp.tables.add('post'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('Users'); //cc
 mobileApp.tables.add('client');
+mobileApp.tables.add('Fridge');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
